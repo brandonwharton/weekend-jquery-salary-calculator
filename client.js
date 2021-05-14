e71feb6bbc18c1c12c65cb340cc45d3a52073469
Brandon Wharton <whartonbm@gmail.com>
2021-05-14 15:33:02 -0500

    AInputs now create an employee object, cleared input fields, stored employee object

--- client.js
+++ client.js
@@ -2,12 +2,40 @@ console.log('js running');
 
 $(document).ready(readyNow);
 
+// hold employee objects
+const employees = [];
 
+
+function addEmployee() {
+    // get values from inputs on DOM and create employee object with them
+    const employee = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        idNumber: $('#empId').val(),
+        title: $('#empTitle').val(),
+        salary: $('#empSalary').val()
+    }
+
+    // clear inputs
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#empId').val('');
+    $('#empTitle').val('');
+    $('#empSalary').val('');
+
+    // push employee object to employees array
+    employees.push(employee);
+    
+    console.log(employees);
+    
+}
 
 
 
 
 function readyNow() {
     console.log('jquery running');
-    
+
+    // click listeners
+    $('#submit').on('click', addEmployee)
 }
